009d0b71185437dfc0dca4dacbda29578c3786cf
davdev14 <davdev1400@gmail.com>
2024-11-05 17:16:17 +0100

    coffee service injected in controller

--- src/coffees/coffees.controller.ts
+++ src/coffees/coffees.controller.ts
@@ -1,47 +1,49 @@
 /* eslint-disable prettier/prettier */
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
 } from '@nestjs/common';
 import { CoffeesService } from './coffees.service';
 
 @Controller('coffees')
 export class CoffeesController {
-
     constructor(private readonly coffeeService: CoffeesService) {}
 
-  @Get()
-  findAll(@Query() paginationQuery) {
-    const { limit, offset } = paginationQuery;
-    return `this action returns all of the coffees. Limit: ${limit}, Offset: ${offset}`;
-  }
+    @Get()
+    findAll(@Query() paginationQuery) {
+        // const { limit, offset } = paginationQuery;
+        return this.coffeeService.findAll();
+    }
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return `this action returns #${id} coffee`;
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.coffeeService.findOne(id);
+    }
 
-  @Post()
-  create(@Body() body) {
-    return body;
-  }
+    @Post()
+    create(@Body() body) {
+        const lastItem = this.coffeeService.findAll();
+        return this.coffeeService.create({
+            id: lastItem[0].id++,
+            name: body.name,
+            brand: body.brand,
+            flavours: body.flavours,
+        });
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() body) {
-    return {
-      message: `this action updates #${id} coffee`,
-      body,
-    };
-  }
+    @Patch(':id')
+    update(@Param('id') id: string, @Body() body) {
+        return this.coffeeService.update(id, body);
+    }
 
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return `this action deletes #${id} coffee`;
-  }
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.coffeeService.remove(id);
+    }
 }
--- src/coffees/entities/coffee.entity.ts
+++ src/coffees/entities/coffee.entity.ts
@@ -1,6 +1,6 @@
 export class Coffee {
-  id: number;
-  name: string;
-  brand: string;
-  flavours: string[];
+    id: number;
+    name: string;
+    brand: string;
+    flavours: string[];
 }
