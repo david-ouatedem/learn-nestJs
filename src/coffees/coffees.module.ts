f8beb2c703148492b1ef360c67088a0e910898b1
davdev14 <davdev1400@gmail.com>
2024-11-21 10:07:26 +0100

    added validations on dtos

--- src/coffees/coffees.module.ts
+++ src/coffees/coffees.module.ts
@@ -1,4 +1,3 @@
-/* eslint-disable prettier/prettier */
 import { Module } from '@nestjs/common';
 import { CoffeesController } from './coffees.controller';
 import { CoffeesService } from './coffees.service';
--- src/coffees/dto/create-coffee.dto.ts
+++ src/coffees/dto/create-coffee.dto.ts
@@ -1,6 +1,10 @@
-/* eslint-disable prettier/prettier */
+import { IsString } from 'class-validator';
+
 export class CreateCoffeeDto {
+    @IsString()
     readonly name: string;
+    @IsString()
     readonly brand: string;
+    @IsString({ each: true })
     readonly flavours: string[];
 }
